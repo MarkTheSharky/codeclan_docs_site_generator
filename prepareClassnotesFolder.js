0d63fa453383ae5a4f663e72ce41da1d82915725
Mark Burns <77061384+MarkTheSharky@users.noreply.github.com>
2022-05-11 20:56:13 +0100

    moved copyLessons functions to prepareClassnotes file

--- prepareClassnotesFolder.js
+++ prepareClassnotesFolder.js
@@ -1,4 +1,37 @@
 const fs = require('fs')
+const path = require('path')
+
+function copyLessonFiles(filepath) {
+
+    fs.readdirSync(filepath).forEach(file => {
+
+        const stat = fs.statSync(`${filepath}/${file}`)
+
+        // Handle if directory
+        if (stat.isDirectory()) {
+            copyLessonFiles(`${filepath}/${file}`)
+        }
+
+        // Handle if a file
+        if (stat.isFile() && file.endsWith('.md') && file.toLowerCase() !== 'readme.md') {
+
+            const tempDir = `${filepath}/${file}`.substring(0, `${filepath}/${file}`.indexOf('/', 19))
+            const newDir = 'app/docs/codeclan/' + tempDir.slice(11)
+
+            if (!fs.existsSync(newDir)) {
+                fs.mkdirSync(newDir, { recursive: true })
+            }
+
+            const src = path.join(__dirname, `${filepath}/${file}`)
+            const newFile = `${filepath}/${file}`.substring(0, `${filepath}/${file}`.indexOf('/', 19)) + `/${file}`
+            const dest = 'app/docs/codeclan' + newFile.slice(10)
+
+            if (!fs.existsSync(dest)) {
+                fs.copyFileSync(src, dest, fs.constants.COPYFILE_EXCL)
+            }
+        }
+    })
+}
 
 
 function prepareFiles(filepath) {
@@ -34,6 +67,26 @@ function prepareFiles(filepath) {
         console.log('Removed .git folder');
     }
 
+// Copy lessons to Vuepress Docs
+
+    copyLessonFiles('classnotes')
+
+    console.log('Copied Lesson files to ./app/docs/codeclan');
+
+// Create empty Readme.md files in week folders
+
+    const readmePath = 'app/docs/codeclan'
+
+    fs.readdirSync(readmePath).forEach(file => {
+        
+        const readme = `${readmePath}/${file}/README.md`
+
+        if (!fs.existsSync(readme) && fs.statSync(`${readmePath}/${file}`).isDirectory()) {
+            fs.writeFileSync(readme, '')
+        }
+    })
+
+    console.log('README files created successfully.')
 }
 
 
