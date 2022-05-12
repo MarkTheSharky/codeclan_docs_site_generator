6a471b6be215fc36c1d5961145243366a6b79ec2
Mark Burns <77061384+MarkTheSharky@users.noreply.github.com>
2022-05-12 15:27:59 +0100

    removed unused code from config.js

--- app/docs/.vuepress/config.js
+++ app/docs/.vuepress/config.js
@@ -1,9 +1,6 @@
 const buildSidebar = require('./getSidebar.js')
 const makeNavbar = require('./getNavbar.js')
 
-// const sidebar = new Sidebar()
-// sidebar.build('docs/codeclan')
-
 module.exports = {
     // site config
   lang: 'en-US',
@@ -21,38 +18,6 @@ module.exports = {
       },
     ],
     sidebarDepth: 0,
-    // sidebar: {
-    //   '/codeclan/week_01/': [
-    //     {
-    //       text: 'Week 1',
-    //       children: [
-    //         {
-    //           text: 'Day 1',
-    //           children: [
-    //             {
-    //               text: 'Command Line Basics',
-    //               children: [
-    //                 '/codeclan/week_01/day_1/command_line_basics/command_line_basics.md',
-    //                 '/codeclan/week_01/day_1/command_line_basics/terminal_cheatsheet.md'
-    //               ],
-    //             },
-    //             '/codeclan/week_01/day_1/homework.md',
-    //             '/codeclan/week_01/day_1/what_is_programming.md'
-    //           ]
-    //         },
-    //         {
-    //           text: 'Day 2',
-    //           children: [
-    //             '/codeclan/week_01/day_1/homework.md',
-    //             '/codeclan/week_01/day_1/what_is_programming.md'
-    //           ]
-    //         },
-    //         '/codeclan/week_01/weekend_homework.md'
-    //       ]
-    //     },
-    //   ]
-    // },
-    // sidebar: sidebar['children'],
     sidebar: buildSidebar('docs/codeclan'),
     lastUpdated: false,
     contributors: false,
@@ -64,7 +29,3 @@ module.exports = {
     ],
   ],
 }
-
-// console.dir(sidebar['children'], { depth : 2 });
-// console.dir(navbar['children'], { depth : null });
-// console.log(makeNavbar('docs/codeclan'));
