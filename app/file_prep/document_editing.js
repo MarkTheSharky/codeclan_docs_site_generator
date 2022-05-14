6f21b4b352e724f061a35186dc0048a0b403ea04
Mark Burns <77061384+MarkTheSharky@users.noreply.github.com>
2022-05-14 01:47:30 +0100

    RegEx to find and replace markdown image links, WIP

--- app/file_prep/document_editing.js
+++ app/file_prep/document_editing.js
@@ -15,34 +15,40 @@ keys.forEach(key => {
 
 
 // Change </br> tags to <br>
-const options = {
-  files: fileArray,
-  from: [/<\/br>/g],
-  to: ['<br>'],
-  countMatches: true,
-}
-
-replace(options)
-  .then(changedFiles => {
-    // fs.writeFileSync('files.json', JSON.stringify(changedFiles, null, 4))
-    console.log('Modified files:', changedFiles.join(', '));
-  })
-  .catch(error => {
-    console.error('Error occurred:', error);
-  });
-
 // const options = {
-//   files: 'app/docs/codeclan/week_01/day_1/command_line_basics_test.md',
-//   from: '',
+//   files: fileArray,
+//   from: [/<\/br>/g],
 //   to: ['<br>'],
 //   countMatches: true,
-//   dry: true,
 // }
 
 // replace(options)
 //   .then(changedFiles => {
+//     // fs.writeFileSync('files.json', JSON.stringify(changedFiles, null, 4))
 //     console.log('Modified files:', changedFiles.join(', '));
 //   })
 //   .catch(error => {
 //     console.error('Error occurred:', error);
 //   });
+
+
+
+const originalFolder = (args) => {
+  return files[args.replace('..', 'app')].originalFolder
+}
+
+
+const options = {
+  files: '../docs/codeclan/week_01/day_2/conditionals.md',
+  from: /!\[([^)]+)\]\(([^)]+)\)/,
+  to: (match, p1, p2, ...args) => match.replace(p2, `${originalFolder(args.pop())}${p2}`),
+  countMatches: true,
+}
+
+replace(options)
+  .then(changedFiles => {
+    console.log('Modified files:', changedFiles.join(', '));
+  })
+  .catch(error => {
+    console.error('Error occurred:', error);
+  });
