a18ecf0973456438aa911ad2521c3427343fb6a6
Mark Burns <markburns@gmail.com>
2022-05-14 02:34:14 +0100

    function to make image link

--- app/file_prep/document_editing.js
+++ app/file_prep/document_editing.js
@@ -2,7 +2,6 @@ const fs = require('fs')
 const replace = require('replace-in-file')
 const files = require('../../files.json')
 
-
 // Array of lesson files
 const fileArray = [];
 
@@ -13,6 +12,20 @@ keys.forEach(key => {
 })
 
 
+const makeImageLink = (file, link) => {
+  const i = link.indexOf('i')
+  const sliced = link.slice(i);
+  return `${files[file.replace('..', 'app')].originalFolder}/${sliced}`
+}
+
+
+const options = {
+  files: '../docs/codeclan/week_01/day_2/conditionals.md',
+  from: /!\[([^)]+)\]\(([^)]+)\)/g,
+  to: (match, p1, p2, ...args) => match.replace(p2, makeImageLink(args.pop(), p2)),
+  countMatches: true,
+}
+
 
 // Change </br> tags to <br>
 // const options = {
@@ -22,31 +35,10 @@ keys.forEach(key => {
 //   countMatches: true,
 // }
 
-// replace(options)
-//   .then(changedFiles => {
-//     // fs.writeFileSync('files.json', JSON.stringify(changedFiles, null, 4))
-//     console.log('Modified files:', changedFiles.join(', '));
-//   })
-//   .catch(error => {
-//     console.error('Error occurred:', error);
-//   });
-
-
-
-const originalFolder = (args) => {
-  return files[args.replace('..', 'app')].originalFolder
-}
-
-
-const options = {
-  files: '../docs/codeclan/week_01/day_2/conditionals.md',
-  from: /!\[([^)]+)\]\(([^)]+)\)/,
-  to: (match, p1, p2, ...args) => match.replace(p2, `${originalFolder(args.pop())}${p2}`),
-  countMatches: true,
-}
 
 replace(options)
   .then(changedFiles => {
+    // fs.writeFileSync('files.json', JSON.stringify(changedFiles, null, 4))
     console.log('Modified files:', changedFiles.join(', '));
   })
   .catch(error => {
