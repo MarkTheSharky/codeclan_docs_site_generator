41d7c5ec47bd8e29d80aec8246e1e62259e0ff89
Mark Burns <77061384+MarkTheSharky@users.noreply.github.com>
2022-05-31 16:18:42 +0100

    Refactor: removed __dirpath from handleClassnotesFolder.js

--- app/docs_prep/lib/handleClassnotesFolder.js
+++ app/docs_prep/lib/handleClassnotesFolder.js
@@ -8,21 +8,24 @@ function handleClassnotes(filepath) {
     const classnotesFolder = fs.readdirSync(filepath).filter(file => file.charAt(0) !== '.')
 
     if (classnotesFolder.length === 1) {
+        
         const folders = fs.readdirSync(`${filepath}/${classnotesFolder}`)
 
         folders.forEach(folder => {
-            const oldPath = `${__dirname}/${filepath}/${classnotesFolder}/${folder}`
-            const newPath = `${__dirname}/${filepath}/${folder}`
+            const oldPath = `${filepath}/${classnotesFolder}/${folder}`
+            const newPath = `${filepath}/${folder}`
             fs.renameSync(oldPath, newPath)
         })
+
+        console.log('Moved folders');
+        
     } else {
         console.log(`Unable to deconsolidate classnotes folder`);
     }
-    console.log('Moved folders');
 
     // Remove old root folder
 
-    if (classnotesFolder.length === 1) {
+    if (classnotesFolder.length === 1) { // This could be improved, will not run after the first time
         fs.rmSync(`${filepath}/${classnotesFolder}`, { recursive: true })
         console.log(`Removed empty ${classnotesFolder} folder`);
     }
