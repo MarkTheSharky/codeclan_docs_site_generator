9e3ddbf1f5527d1e5fe3eedb7d2499f469ba616d
Mark Burns <77061384+MarkTheSharky@users.noreply.github.com>
2022-06-02 02:51:57 +0100

    Added from and to function to handle cases in specific files

--- app/docs_prep/lib/markdownEditing.js
+++ app/docs_prep/lib/markdownEditing.js
@@ -21,6 +21,27 @@ function editMarkdownFiles(obj) {
 		}
 	}
 
+	const from = (file) => {
+		switch (file.match(/app\/docs\/codeclan\/.*/gi)[0]) {
+			case 'app/docs/codeclan/week_11/day_2/arrays_and_arraylists.md':
+				return /(?<=So )ArrayList<Integer>(?= would)/i
+			// case
+			default:
+				return /[]/
+		}	
+	}
+
+	const to = (match) => {
+		switch (match) {
+			case 'ArrayList<Integer>':
+				return '`ArrayList<Integer>`'
+			// case
+			default:
+				console.log('Error in "to" callback, default called and match copied back');
+				return match
+		}	
+	}
+
 	// Add any changes to markdown files below. Numbered keys are used for readability between the from/to values.
 		// i.e. Value at key 1 in fromObj will be changed to value at key 1 in toObj and so on.
 		
@@ -28,9 +49,10 @@ function editMarkdownFiles(obj) {
 
 	const fromObj = {
 					1: /<\/br>/g,
-					2: /{{ ... }}/g,
+					2: /(?<!`){{ ... }}(?!`)/g,
 					3: /<%/g,
 					4: /!\[([^)]+|)\]\(([^)]+)\)/g,
+					5: from
 					}
 
 	const toObj = {
@@ -38,23 +60,33 @@ function editMarkdownFiles(obj) {
 					2: '`{{ ... }}`',
 					3: '&lt;%',
 					4: imageLinkFix,
+					5: to,
 				}
 
 
 	const options = {
+		dry: true,			/////////////////////////// Disable to run!!!!!!!
 		files: keyArray(obj),
 		from: Object.values(fromObj),
 		to: Object.values(toObj),
 		countMatches: true,
 	}
 
+function makeChangedFile(results) {
+	const changedFiles = results
+			.filter(result => result.hasChanged)
+			.map(result => result.file);
+	console.log(changedFiles);
+}
+
 	replace(options)
 		.then(changedFiles => {
-		console.log('Completed files change');
-		// console.log(changesFiles);
+			makeChangedFile(changedFiles)
+			console.log('Completed files change');
+			// console.log(changesFiles);
 		})
 		.catch(error => {
-		console.error('Error occurred:', error);
+			console.error('Error occurred:', error);
 		});
 }
 
