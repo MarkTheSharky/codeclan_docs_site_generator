9ac4dd80144e11678e95dbcd0a66f8d36db619df
Mark Burns <markburns@gmail.com>
2022-06-17 00:12:26 +0100

    Added launchServer to pagesCheck and configed url to check

--- app/docs_prep/lib/checkForPageErrors.js
+++ app/docs_prep/lib/checkForPageErrors.js
@@ -1,28 +1,42 @@
+const path = require('path')
 const fetch = require('node-fetch')
 const filesJSON = require('../data/files.json')
+const { launchDevServer } = require('./createDevServer') 
 
 
 async function checkForPageErrors() {
 
-    // start server using server-start-test
+    // start dev server
+    await launchDevServer()
+
+    console.log('Dev Server Launched');
 
     const fileList = Object.keys(filesJSON);
 
+    console.log('Checking Pages For Errors');
+
     const statuses = await Promise.all(fileList.map((file) => {
-        return fetch(`http://localhost:8080/${filesJSON[file].url}`)
+        const url = path.join('http://localhost:8080/@fs', __dirname, '..', '..', '..', `app/docs/.vuepress/.temp/pages/${filesJSON[file].url}.vue`)
+
+        return fetch(url)
             .then(res => { return [res.status, file] })
+            .catch(err => console.error(err) )
     }))
 
     const errorPages = []
 
     const result = statuses.forEach((pageStatus) => {
         if (pageStatus[0] !== 200) {
-            errorPages.push(pageStatus[1])
+            errorPages.push(pageStatus)
         }
     })
 
-    return errorPages
+    console.log('Checking Complete');
+    // return errorPages
+    console.log(errorPages)
     
 }
+
+checkForPageErrors()
 
 module.exports = checkForPageErrors
