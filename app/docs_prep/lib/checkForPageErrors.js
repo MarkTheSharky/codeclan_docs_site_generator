521f2f9541214b40ff920be1dc6e3a21f5404970
Mark Burns <77061384+MarkTheSharky@users.noreply.github.com>
2022-06-19 20:12:20 +0100

    Fixes to function plus added write results to file

--- app/docs_prep/lib/checkForPageErrors.js
+++ app/docs_prep/lib/checkForPageErrors.js
@@ -1,15 +1,13 @@
+const fs = require('fs')
 const path = require('path')
 const fetch = require('node-fetch')
 const filesJSON = require('../data/files.json')
 const { launchDevServer } = require('./createDevServer') 
 
-
 async function checkForPageErrors() {
 
     // start dev server
     await launchDevServer()
-
-    console.log('Dev Server Launched');
 
     const fileList = Object.keys(filesJSON);
 
@@ -20,23 +18,28 @@ async function checkForPageErrors() {
 
         return fetch(url)
             .then(res => { return [res.status, file] })
-            .catch(err => console.error(err) )
+            // Connection errors produced as checkForPageErrors is called from another file, re run fetch /// Temp fix!!!
+            .catch(res => fetch(url)
+                    .then(res => { return [res.status, file] })
+                    .then(console.log(url))
+                    .catch(err => console.error(err) ))
     }))
 
     const errorPages = []
 
-    const result = statuses.forEach((pageStatus) => {
-        if (pageStatus[0] !== 200) {
+    statuses.forEach((pageStatus) => {
+        if (!pageStatus) { // Handles undefined
+            console.log(pageStatus);
+            return
+        } else if (pageStatus[0] !== 200) {
             errorPages.push(pageStatus)
         }
     })
 
+    fs.writeFileSync(path.join(__dirname, '..', 'data', 'fileErrors.txt'), errorPages.join('\n'))
     console.log('Checking Complete');
-    // return errorPages
-    console.log(errorPages)
-    
 }
 
-checkForPageErrors()
+// checkForPageErrors()
 
-module.exports = checkForPageErrors
+module.exports = { checkForPageErrors }
