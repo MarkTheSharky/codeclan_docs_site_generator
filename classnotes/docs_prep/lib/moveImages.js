2ae377d02ef7547d8c3643fabdf7e5ca8ff4bea1
Mark Burns <77061384+MarkTheSharky@users.noreply.github.com>
2024-04-24 15:30:25 +0100

    Removed classnotes + docs_prep to implement in npx package instead

--- classnotes/docs_prep/lib/moveImages.js
+++
@@ -1,4 +0,0 @@
-// SUDO CODE
-// Filter through all folder/files in e46_classnotes looking for files with ext (jpg, jpeg, png, etc.)
-// If found copy to new location called images
-// Make object of file name and path
